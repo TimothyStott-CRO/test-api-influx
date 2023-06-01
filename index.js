20a58cfa00f49cd921a6e312618302e8686323d3
Tim <timothy.b.stott@gmail.com>
2023-06-01 15:31:40 -0400

    Add email post request

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import { InfluxDB } from '@influxdata/influxdb-client'
+import bodyParser from 'body-parser'
 import express from 'express'
 const app = express()
-app.use(function (req, res, next) {
+app.use(async function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+const jsonParser = bodyParser.json()
 const port = 3001
+
+function createQueryClient(url, token, org) {
+    const client = new InfluxDB({ url, token })
+    const queryClient = client.getQueryApi(org)
+    return queryClient
+}
+
 
 //cloud bucket
 const token = "_Vjg-EobTLO-PRE2_7oed8kI9tL3mA6gTMMClcq2Gu0J5orMD_ilTZ0oTtJVNS4fStULh3nYB0BEsNb6gDej2Q==";
 const org = "Onsrud";
 const url = "http://3.15.33.121:8086";
 
-// const org = "CRO";
-// const token = "Ghooo4rBGx61mJpO3wFUduX1jg84TI5wGS6uyjsSEIfMwlTM27KrcgBBXjrXFXEjAwXNyol0LifOeZ5-Bl-_9Q==";
 
 
 // //local DB
@@ -218,8 +225,8 @@ app.get('/historical-loads-and-temps/:bucket/:range?/', (req, res) => {
 
 app.get('/alarms-details/:bucket/:time/', (req, res) => {
     let bucket = req.params['bucket'];
-    var startTime = (new Date((Date.parse(req.params['time'])) - (15 * 60 * 1000))).toISOString();
-    var endTime = (new Date((Date.parse(req.params['time'])) + (15 * 60 * 1000))).toISOString();
+    var startTime = (new Date((Date.parse(req.params['time'])) - (5 * 60 * 1000))).toISOString();
+    var endTime = (new Date((Date.parse(req.params['time'])) + (5 * 60 * 1000))).toISOString();
 
 
     var dailyAlarmQuery = `from(bucket: "${bucket}")
@@ -245,8 +252,8 @@ app.get('/alarms-details/:bucket/:time/', (req, res) => {
 
 app.get('/alarms-details-sm/:bucket/:time/', (req, res) => {
     let bucket = req.params['bucket'];
-    var startTime = (new Date((Date.parse(req.params['time'])) - (15 * 60 * 1000))).toISOString();
-    var endTime = (new Date((Date.parse(req.params['time'])) + (15 * 60 * 1000))).toISOString();
+    var startTime = (new Date((Date.parse(req.params['time'])) - (10 * 60 * 1000))).toISOString();
+    var endTime = (new Date((Date.parse(req.params['time'])) + (5 * 60 * 1000))).toISOString();
 
 
     var smQuery = `from(bucket: "${bucket}")
@@ -273,8 +280,8 @@ app.get('/alarms-details-sm/:bucket/:time/', (req, res) => {
 
 app.get('/alarms-details-lt/:bucket/:time/', (req, res) => {
     let bucket = req.params['bucket'];
-    var startTime = (new Date((Date.parse(req.params['time'])) - (15 * 60 * 1000))).toISOString();
-    var endTime = (new Date((Date.parse(req.params['time'])) + (15 * 60 * 1000))).toISOString();
+    var startTime = (new Date((Date.parse(req.params['time'])) - (5 * 60 * 1000))).toISOString();
+    var endTime = (new Date((Date.parse(req.params['time'])) + (5 * 60 * 1000))).toISOString();
 
 
     var smQuery = `from(bucket: "${bucket}")
@@ -300,8 +307,8 @@ app.get('/alarms-details-lt/:bucket/:time/', (req, res) => {
 
 app.get('/alarms-details-os/:bucket/:time/', (req, res) => {
     let bucket = req.params['bucket'];
-    var startTime = (new Date((Date.parse(req.params['time'])) - (15 * 60 * 1000))).toISOString();
-    var endTime = (new Date((Date.parse(req.params['time'])) + (15 * 60 * 1000))).toISOString();
+    var startTime = (new Date((Date.parse(req.params['time'])) - (.15 * 60 * 1000))).toISOString();
+    var endTime = (new Date((Date.parse(req.params['time'])) + (.15 * 60 * 1000))).toISOString();
 
 
     var smQuery = `from(bucket: "${bucket}")
@@ -325,10 +332,261 @@ app.get('/alarms-details-os/:bucket/:time/', (req, res) => {
     })
 })
 
+app.get('/alarms-details-progam-details/:bucket/:time/', (req, res) => {
+    let bucket = req.params['bucket'];
+    var startTime = (new Date((Date.parse(req.params['time'])) - (3 * 60 * 1000))).toISOString();
+    var endTime = (new Date((Date.parse(req.params['time'])) + (3 * 60 * 1000))).toISOString();
+
+
+    var programQuery = `from(bucket: "${bucket}")
+    |> range(start: ${startTime}, stop: ${endTime})
+    |> filter(fn: (r) => r["_measurement"] == "Gen Info")
+    |> filter(fn: (r) => r["_field"] == "Program Line")`
+
+
+    let tableObject;
+    let retArr = [];
+    queryClient.queryRows(programQuery, {
+        next: (row, tableMeta) => {
+            tableObject = tableMeta.toObject(row)
+            retArr.push(tableObject)
+        },
+        error: (error) => {
+            console.log(error)
+        },
+        complete: () => {
+            res.send(retArr)
+        }
+    })
+})
+
+app.post('/email-info/', jsonParser, (req, res) => {
+    const userToken = req.body.token;
+    const userOrg = req.body.org;
+    const buckets = req.body.buckets;
+    // const queryClient = new InfluxDB({ url, userToken }).getQueryApi(userOrg)
+    const queryClient = createQueryClient(url, userToken, userOrg)
+
+    async function aggregateData(buckets) {
+        const unstructuredMachineHours = [];
+        const unstructuredRunTime = [];
+        const unstructuredCycleTime = [];
+        let structuredReturnData = [];
+
+        for (const bucket of buckets) {
+            try {
+                const retMH = await getMachineHoursData(bucket)
+                unstructuredMachineHours.push(retMH)
+                const retRT = await getRunTimeData(bucket)
+                unstructuredRunTime.push(retRT)
+                const retCT = await getCycleTimeData(bucket)
+                unstructuredCycleTime.push(retCT)
+                console.log(bucket)
+                console.log("Productivity: " + calculateProductivity(retRT, retMH))
+                console.log("Total M Time: " + calculateMachineHoursTotal(retMH))
+                console.log("Total Cycle Time: " + calculateTotalCycleTime(retCT))
+                console.log("Average Cycle Time: " + calculateAverageCycleTime(retCT))
+                let productivity = calculateProductivity(retRT, retMH)
+                let totalOnTime = calculateMachineHoursTotal(retMH)
+                let totalCycleTime = calculateTotalCycleTime(retCT)
+                let averageCycleTime = calculateAverageCycleTime(retCT)
+
+                let emailData = {
+                    bucket: bucket,
+                    productivity: productivity,
+                    totalOnTime: totalOnTime,
+                    totalCycleTime: totalCycleTime,
+                    averageCycleTime: averageCycleTime
+                }
+                structuredReturnData.push(emailData)
+            }
+            catch (err) {
+                console.log(err)
+            }
+
+        }
+
+
+
+
+        res.send(structuredReturnData)
+    }
+    aggregateData(buckets)
+
+    async function getMachineHoursData(bucketToGet) {
+        const machineHoursQuery = `from(bucket: "${bucketToGet}")
+        |> range(start: today())
+        |> filter(fn: (r) => r["_measurement"] == "Gen Info")
+        |> filter(fn: (r) => r["_field"] == "Machine Hours")`;
+        const retArr = [];
+        const machineHoursRetObj = await queryClient.collectRows(machineHoursQuery)
+        for (let i = 0; i < machineHoursRetObj.length; i++) {
+            const machineHoursObj = {
+                bucket: bucketToGet,
+                _field: machineHoursRetObj[i]._field,
+                _value: machineHoursRetObj[i]._value,
+                _time: machineHoursRetObj[i]._time,
+                _wasOneShot: machineHoursRetObj[i]["Alarm One Shot"]
+            }
+            if (machineHoursObj._wasOneShot === undefined) {
+                retArr.push(machineHoursObj)
+            }
+        }
+
+        return retArr
+    }
+    async function getRunTimeData(bucketToGet) {
+        const runTimeQuery = `from(bucket: "${bucketToGet}")
+        |> range(start: today())
+        |> filter(fn: (r) => r["_measurement"] == "Gen Info")
+        |> filter(fn: (r) => r["_field"] == "Run Time")`;
+        const retArr = [];
+        const runTimeRetObj = await queryClient.collectRows(runTimeQuery)
+        for (let i = 0; i < runTimeRetObj.length; i++) {
+            const runTimeObj = {
+                bucket: bucketToGet,
+                _field: runTimeRetObj[i]._field,
+                _value: runTimeRetObj[i]._value,
+                _time: runTimeRetObj[i]._time,
+                _wasOneShot: runTimeRetObj[i]["Alarm One Shot"]
+            }
+            if (runTimeObj._wasOneShot === undefined) {
+                retArr.push(runTimeObj)
+            }
+        }
+        return retArr
+    }
+    async function getCycleTimeData(bucketToGet) {
+        const cycleTimeQuery = `from(bucket: "${bucketToGet}")
+        |> range(start: today())
+        |> filter(fn: (r) => r["_measurement"] == "Gen Info")
+        |> filter(fn: (r) => r["_field"] == "Cycle Time")`;
+        const retArr = [];
+        const cycleTimeRetObj = await queryClient.collectRows(cycleTimeQuery)
+        for (let i = 0; i < cycleTimeRetObj.length; i++) {
+            const cycleTimeObj = {
+                bucket: bucketToGet,
+                _field: cycleTimeRetObj[i]._field,
+                _value: cycleTimeRetObj[i]._value,
+                _time: cycleTimeRetObj[i]._time,
+                _wasOneShot: cycleTimeRetObj[i]["Alarm One Shot"]
+            }
+            if (cycleTimeObj._wasOneShot === undefined) {
+                retArr.push(cycleTimeObj)
+            }
+        }
+        return retArr
+    }
+
+
+    function calculateTotalCycleTime(cycleTimesArray) {
+        let totalInSeconds = 0;
+        let secondsArray = [];
+        if (cycleTimesArray.length === 0) {
+            return 0
+        }
+        if (cycleTimesArray.length === 1) {
+            return cycleTimesArray[0]._value
+        }
+        for (let i = 0; i < cycleTimesArray.length; i++) {
+
+            if (i === cycleTimesArray.length - 1 && secondsArray.length === 0) {
+                return cycleTimesArray[i]._value
+            }
+
+
+            if (i !== 0 && cycleTimesArray[i]._value < cycleTimesArray[i - 1]._value) {
+                //turn the cycle time into seconds
+                secondsArray.push(Math.round((Math.floor(cycleTimesArray[i - 1]._value) * 60) + ((cycleTimesArray[i - 1]._value % 1) * 100)))
+            }
+        }
+        for (let i = 0; i < secondsArray.length; i++) {
+            totalInSeconds += secondsArray[i]
+        }
+
+        const hours = Math.floor(totalInSeconds / 60 / 60);
+        const minutes = Math.floor(totalInSeconds / 60) - (hours * 60);
+        const seconds = totalInSeconds % 60;
+        if (hours === 0 && minutes === 0) {
+            return seconds + "s";
+        } else if (hours === 0) {
+            return minutes + "m " + seconds + "s";
+        } else {
+            return hours + "h " + minutes + "m " + seconds + "s";
+        }
+    }
+    function calculateAverageCycleTime(cycleTimesArray) {
+        let totalInSeconds = 0;
+        let secondsArray = [];
+        let averageInSeconds = 0;
+        if (cycleTimesArray.length === 0) {
+            return 0
+        }
+        if (cycleTimesArray.length === 1) {
+            return cycleTimesArray[0]._value
+        }
+        for (let i = 0; i < cycleTimesArray.length; i++) {
+
+            if (i === cycleTimesArray.length - 1 && secondsArray.length === 0) {
+                return cycleTimesArray[i]._value
+            }
+
+
+            if (i !== 0 && cycleTimesArray[i]._value < cycleTimesArray[i - 1]._value) {
+                //turn the cycle time into seconds
+                secondsArray.push(Math.round((Math.floor(cycleTimesArray[i - 1]._value) * 60) + ((cycleTimesArray[i - 1]._value % 1) * 100)))
+            }
+        }
+        for (let i = 0; i < secondsArray.length; i++) {
+            totalInSeconds += secondsArray[i]
+        }
+        averageInSeconds = Math.floor(totalInSeconds / secondsArray.length)
+
+        const hours = Math.floor(averageInSeconds / 60 / 60);
+        const minutes = Math.floor(averageInSeconds / 60) - (hours * 60);
+        const seconds = averageInSeconds % 60;
+        if (hours === 0 && minutes === 0) {
+            return seconds + "s";
+        } else if (hours === 0) {
+            return minutes + "m " + seconds + "s";
+        } else {
+            return hours + "h " + minutes + "m " + seconds + "s";
+        }
+    }
+    function calculateProductivity(runTimesArray, machineHoursArray) {
+        if (runTimesArray.length === 0 || machineHoursArray.length === 0) {
+            return 0
+        }
+        if (runTimesArray.length === 1 || machineHoursArray.length === 1) {
+            return 0
+        }
+        let runTimeTotal = runTimesArray[runTimesArray.length - 1]._value - runTimesArray[0]._value;
+        let machineHoursTotal = machineHoursArray[machineHoursArray.length - 1]._value - machineHoursArray[0]._value;
+        let productivityValue = runTimeTotal / machineHoursTotal;
+        return ((productivityValue.toFixed(2)) * 100);
+    }
+    function calculateMachineHoursTotal(machineHoursArray) {
+        if (machineHoursArray.length === 0) {
+            return 0
+        }
+        if (machineHoursArray.length === 1) {
+            return 0
+        }
+        let machineHoursTotal = machineHoursArray[machineHoursArray.length - 1]._value - machineHoursArray[0]._value;
+        const hours = Math.floor(machineHoursTotal / 60);
+        const minutes = Math.floor(machineHoursTotal % 60);
+        if (hours === 0) {
+            return minutes + "m";
+        } else {
+            return hours + "h " + minutes + "m";
+        }
+    }
+})
 
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
 
 
